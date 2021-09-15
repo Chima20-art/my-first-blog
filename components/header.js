4218aa691999b3231802a28c43d598ee259d96bf
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-09-15 21:04:36 +0300

    modifications added to Header component.

--- components/header.js
+++ components/header.js
@@ -6,6 +6,9 @@ const header = () => {
       <h1 className={headerStyles.title}>
         <span>WebDev</span>News
       </h1>
+      <p className={headerStyles.description}>
+        keep up to date with the latest web dev news
+      </p>
     </div>
   );
 };
