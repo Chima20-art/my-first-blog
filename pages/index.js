656a41eeca6c8140e74d4ff488e9b835c961bdbb
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-09-15 22:05:35 +0300

    Head tag added to index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>webDev Newz</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
       <h1>welcome to next</h1>
     </div>
   );
