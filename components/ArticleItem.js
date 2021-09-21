5970103e20e181115d3f1d96abf79c6eade3f77d
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-09-21 17:06:22 +0300

    Link added to ArticleItem component.

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import articleStyles from "../styles/Article.module.css";
 
-const ArticleItem = () => {
-  return <div></div>;
+const ArticleItem = (article) => {
+  return (
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <a className={articleStyles.card}>
+        <h3>{article.articleStyles}</h3>
+      </a>
+    </Link>
+  );
 };
 
 export default ArticleItem;
